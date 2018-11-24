7e863af3930b6e102dda00ce411c15a7662b1788
Rudie Dirkx <github@hotblocks.nl>
2018-11-25 00:24:19 +0100

    site sorting+

--- rweb.helpers.js
+++ rweb.helpers.js
@@ -103,13 +103,27 @@ rweb = {
 	},
 
 	siteSorter: function(a, b) {
-		if (a.host == 'all') return -1;
+		var diff = rweb.siteSorterHost(a, b);
+		if ( diff == 0 ) {
+			diff = rweb.siteSorterUuid(a, b);
+		}
+
+		return diff;
+	},
+
+	siteSorterHost: function(a, b) {
+		if (a.host == b.host) return 0;
+		else if (a.host == 'all') return -1;
 		else if (b.host == 'all') return 1;
 		else if (a.host == 'matches') return -1;
 		else if (b.host == 'matches') return 1;
 		else if (a.host == 'options') return -1;
 		else if (b.host == 'options') return 1;
 		else return a.host < b.host ? -1 : 1;
+	},
+
+	siteSorterUuid: function(a, b) {
+		return a.id < b.id ? -1 : 1;
 	},
 
 	sites: function(host, callback, options) {
